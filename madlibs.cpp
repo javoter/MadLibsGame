fa545808e62e8987c986e097349ca260aa1ea403
javoter <javoter@mail.csuchico.edu>
2019-09-09 17:10:00 -0700

    There once was a blank

--- madlibs.cpp
+++ madlibs.cpp
@@ -20,7 +20,7 @@ cin >> adjective;
 cout << "Enter a different food";
 cin >> differentTypeOfFood;
 
-cout << "There once was a"<<animal<< "walking through a forest looking for some"<<typeOfFood<< "to eat, when there he stumbled upon a"<<adjective<<<<differentTypeOfFood<< "hanging high in a tree, the"<<animal<< "reached high up to grab it only to find it was there imagination."
+cout << "There once was a"<<animal<< "walking through a forest looking for some"<<typeOfFood<< "to eat, when there he stumbled upon a"<<adjective<<" "<<differentTypeOfFood<< "hanging high in a tree, the"<<animal<< "reached high up to grab it only to find it was there imagination.";
 
   return 0;
 }
